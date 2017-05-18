305ffa3a952b9f445e42bf6de566a64c04392873
Your Full Name <youremailaddress@example.com>
2017-05-18 10:57:07 -0400

    font awesome and local storage

--- js/app.js
+++ js/app.js
@@ -6,17 +6,25 @@ const megaroster = {
   init(listSelector) {
     this.studentList = document.querySelector(listSelector)
     this.max = 0
-    this.setupEventListeners()  
+    this.setupEventListeners() 
+    this.load() 
   },
 
   setupEventListeners() {
     document
       .querySelector('#new-student')
-      .addEventListener('submit', this.addStudent.bind(this))
+      .addEventListener('submit', this.addStudentViaForm.bind(this))
   },
 
   save() {
      localStorage.setItem('student', JSON.stringify(this.students))
+  },
+
+  load() {
+    const studentStr = localStorage.getItem('student')
+    const studentArr = JSON.parse(studentStr)
+
+    studentArr.map(this.addStudent.bind(this))
   },
 
   removeStudent(ev) {
@@ -26,11 +34,23 @@ const megaroster = {
     for(let i = 0; i < this.students.length; i++) {
       if(this.students[i].id.toString() === id.dataset.id) {
         this.students.splice(i, 1)
+        break
       }
     }
 
     id.remove()
     this.save()
+  },
+
+  addStudentViaForm(ev) {
+    ev.preventDefault()
+    const f = ev.target
+    const student = {
+      id: this.max + 1,
+      name: f.studentName.value,
+    }
+    this.addStudent(student)
+    f.reset()
   },
 
   promoteStudent(ev) {
@@ -69,20 +89,15 @@ const megaroster = {
     
   },
 
-  addStudent(ev) {
-    ev.preventDefault()
-    const f = ev.target
-    const student = {
-      id: this.max + 1,
-      name: f.studentName.value,
-    }
+  addStudent(student) {
     this.students.unshift(student)
 
     const listItem = this.buildListItem(student)
     this.prependChild(this.studentList, listItem)
 
-    this.max ++
-    f.reset()
+     if (student.id > this.max) {
+      this.max = student.id
+    }
     this.save()
   },
 
